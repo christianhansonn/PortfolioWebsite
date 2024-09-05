92e80519d376f7e66b22f541480a828c702a960c
Christian Hanson <christian.hanson3@gmail.com>
2024-09-04 22:34:59 -0700

    Created Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useRef } from "react";
 import Home from "./components/Home";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   const navRef = useRef(null);
   const homeRef = useRef(null);
   const aboutRef = useRef(null);
-  // TODO: Build Projects modal
+  const projectRef = useRef(null);
 
   const homeScroll = () => homeRef.current.scrollIntoView();
   const aboutScroll = () => aboutRef.current.scrollIntoView();
+  const projectScroll = () => projectRef.current.scrollIntoView();
 
   const navFormat =
     "mx-2 text-sm font-bold hover:cursor-pointer hover:text-black hover:underline";
@@ -28,18 +30,23 @@ function App() {
             <li className={navFormat} onClick={aboutScroll}>
               About
             </li>
-            {/* <li className={navFormat}>Projects</li> */}
+            <li className={navFormat} onClick={projectScroll}>
+              Projects
+            </li>
           </ul>
         </div>
       </nav>
       <div
-        className="max-sm:ml-[70px] ml-20 flex h-screen items-center"
+        className="ml-20 flex h-screen items-center max-sm:ml-[70px]"
         ref={homeRef}
       >
         <Home />
       </div>
       <div className="flex" ref={aboutRef}>
         <About />
+      </div>
+      <div className="ml-4 mt-10 pb-10" ref={projectRef}>
+        <Projects />
       </div>
     </>
   );
---
+++ src/components/Projects.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import pipeline from "/image/data-pipeline.png";
+import webScrape from "/image/webscrape.jpg";
+import machineLearning from "/image/machine-learning.jpg";
+import { twMerge } from "tailwind-merge";
+
+function Card({ children, source, url, gradientClass = "", ...props }) {
+  const navigate = () => {
+    window.open(url, "_blank", "noreferrer");
+  };
+
+  const formatting = twMerge(
+    "absolute inset-0 flex items-end bg-gradient-to-t from-black/60 to-transparent",
+    gradientClass,
+  );
+  return (
+    <div
+      {...props}
+      className="group relative max-w-xs overflow-hidden rounded-2xl shadow-lg max-md:h-[180px] lg:h-[230px]"
+      onClick={navigate}
+    >
+      <img
+        src={source}
+        alt=""
+        className="h-full w-full object-cover transition-transform duration-200 group-hover:scale-110"
+      />
+      <div className={formatting}>
+        <div className="p4 text-white">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default function Projects() {
+  return (
+    <div className="flex items-center justify-center max-[800px]:flex-col">
+      <div className="m-2 max-w-xs cursor-pointer">
+        <Card
+          source={pipeline}
+          url="https://github.com/christianhansonn/PortfolioDataPipeline"
+        >
+          <h3 className="pl-4 font-bold max-md:text-[14px] max-md:leading-4 md:text-sm lg:text-lg">
+            AWS Data Pipeline
+          </h3>
+          <p className="px-4 pb-3 text-xs max-md:text-[12px] max-md:leading-3 md:text-[14px] md:leading-[16px]">
+            This project showcases my abilities process data using a custom
+            Python script and AWS services S3, Glue, and Athena
+          </p>
+        </Card>
+      </div>
+      <div className="m-2 max-w-xs cursor-pointer">
+        <Card
+          source={webScrape}
+          url="https://github.com/christianhansonn/PortfolioWebScrape"
+          gradientClass="from-black/80"
+        >
+          <h3 className="pl-4 font-bold max-md:text-[14px] max-md:leading-4 md:text-sm lg:text-lg">
+            BeautifulSoup Webscrape
+          </h3>
+          <p className="px-4 pb-3 text-xs max-md:text-[12px] max-md:leading-3 md:text-[14px] md:leading-[16px]">
+            This project showcases my abilities to webscrape a job board using
+            BeautifulSoup, and then automate job applications with Playwright
+          </p>
+        </Card>
+      </div>
+      <div className="m-2 max-w-xs cursor-pointer max-[800px]:ml-2">
+        <Card
+          source={machineLearning}
+          url="https://github.com/christianhansonn/PortfolioProject/blob/main/Machine%20Learning%20Predictions.ipynb"
+        >
+          <h3 className="pl-4 font-bold max-md:text-[14px] max-md:leading-4 md:text-sm lg:text-lg">
+            Machine Learning
+          </h3>
+          <p className="px-4 pb-3 text-xs max-md:text-[12px] max-md:leading-3 md:text-[14px] md:leading-[16px]">
+            This project showcases my abilities to create and train three simple
+            machine learning models to predict client conversion and boost
+            retention
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
+}
